45823167b258346f5808c99b7c8e96f928fc75d1
Javier <jlosabalsa@gmail.com>
2020-03-08 12:44:43 +0100

    4.5 e info del perfil de usuario añadidos

--- src/store.js
+++ src/store.js
@@ -1,9 +1,12 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 
 // 4.3 Reducers
 import albumHistory from './reducers/albumHistory';
 import songHistory from './reducers/songHistory';
 import login from './reducers/login';
 
+// 4.5 Middlewares
+import logger from './middlewares/logger';
+
 // 4.2 Store
-export default createStore(combineReducers({ albumHistory, songHistory, login }));
+export default createStore(combineReducers({ albumHistory, songHistory, login }, applyMiddleware(logger)));
